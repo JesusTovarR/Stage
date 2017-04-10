671843ec4b2d52c20b4cbed6abc9862ec7af0d1f
Nayeli Flores <hec_1503@hotmail.com>
2017-04-10 16:29:43 +0200

    invoices

--- connexion.php
+++ connexion.php
@@ -33,13 +33,34 @@ if ($result === false){
     $array = explode("\n", $result);
     $token = explode("=", $array[2]);
     $auth = $token[1];
-    invoices($auth);
+    org($auth);
 }
 
 /*****************************************************************************/
 
-function invoices($auth){
+function org($auth){
     $url='https://invoice.zoho.eu/api/v3/organizations?authtoken='.$auth;
+    $headers = array('Accept' => 'application/json');
+//$options = array('auth' => array('jesustovar678@gmail.com', 'Tovar19011995'));
+    $request = Requests::get($url, $headers/*, $options*/);
+
+    var_dump($request->status_code);
+// int(200)
+
+//    var_dump($request->headers['content-type']);
+// string(31) "application/json; charset=utf-8"
+
+    $data=json_decode($request->body);
+//    var_dump($data);
+
+    $ido=$data->organizations[0]->organization_id;
+// string(26891) "[...]"
+    invoices($auth, $ido );
+}
+
+
+function invoices($auth, $ido){
+    $url='https://invoice.zoho.eu/api/v3/invoices?authtoken='.$auth.'&organization_id='.$ido;
     $headers = array('Accept' => 'application/json');
 //$options = array('auth' => array('jesustovar678@gmail.com', 'Tovar19011995'));
     $request = Requests::get($url, $headers/*, $options*/);
@@ -53,8 +74,6 @@ function invoices($auth){
     $data=json_decode($request->body);
     var_dump($data);
 
-    echo $data->organizations[0]->organization_id;
-// string(26891) "[...]"
 }
 
 
